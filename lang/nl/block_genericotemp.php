a7999def167ab38f11dca3a8050e0f0b4fa57af4
Luuk Verhoeven <luuk@ldesign.nl>
2019-12-30 13:07:02 +0100

    Add NL language file

---
+++ lang/nl/block_genericotemp.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_genericotemp', language 'nl'
+ *
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @package   block_genericotemp
+ * @copyright 29/12/2019 Mfreak.nl | LdesignMedia.nl - Luuk Verhoeven
+ * @author    Luuk Verhoeven
+ */
+$string['pluginname'] = 'Generico template';
+$string['genericotemp:addinstance'] = 'Add a new Generico template block';
+$string['genericotemp:myaddinstance'] = 'Add a new Generico template block to Dashboard';
+$string['genericotemp:view'] = 'View generico template block';
+
+$string['allowadditionalcssclasses'] = 'Allow additional CSS classes';
+$string['configallowadditionalcssclasses'] = 'Adds a configuration option to HTML block instances allowing additional 
+CSS classes to be set.';
+$string['configclasses'] = 'Additional CSS classes';
+$string['configclasses_help'] = 'The purpose of this configuration is to aid with theming by helping distinguish HTML blocks 
+from each other. Any CSS classes entered here (space delimited) will be appended to the block\'s default classes.';
+$string['configcontent'] = 'Content';
+$string['configtitle'] = 'HTML block title';
+$string['newgenericotempblock'] = '(new generico template block)';
+$string['search:content'] = 'Generico template block content';
+$string['privacy:metadata:block'] = 'The Generico template block stores all of its data within the block subsystem.';
+
+// Settings.
+$string['setting:text'] = 'Default content for a new generico template block';
